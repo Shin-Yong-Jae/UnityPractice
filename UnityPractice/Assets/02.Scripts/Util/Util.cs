7acd158efb3968a9f013a4a00b3f3b69593687f1
시뇽_신용재 <sms3284@naver.com>
2022-11-10 20:31:12 +0900

    07. Util (Screen To World, Curve)

--- UnityPractice/Assets/02.Scripts/Util/Util.cs
+++ UnityPractice/Assets/02.Scripts/Util/Util.cs
@@ -42,4 +42,53 @@ public static class Util
             image.color = color;
         }
     }
+
+    public static Vector2 TransWorldPositionToCanvasPostion(Vector2 _worldPostion, Vector2 _canvasSizeDelta)
+    {
+        Vector2 newPos = Camera.main.WorldToViewportPoint(_worldPostion);
+        newPos.x *= _canvasSizeDelta.x;
+        newPos.y *= _canvasSizeDelta.y;
+
+        return newPos;
+    }
+
+    public static Vector2 TransWorldPositionToCanvasPostion(Vector2 _worldPostion, Vector2 _canvasSizeDelta, Vector2 _anchor)
+    {
+        Vector2 newPos = Camera.main.WorldToViewportPoint(_worldPostion);
+        newPos.x *= _canvasSizeDelta.x;
+        newPos.y *= _canvasSizeDelta.y;
+
+        newPos.x -= _canvasSizeDelta.x * _anchor.x;
+        newPos.y -= _canvasSizeDelta.y * _anchor.y;
+
+        return newPos;
+    }
+
+    public static Vector3 GetBezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        float u = 1f - t;
+        float t2 = t * t;
+        float u2 = u * u;
+
+        Vector3 result =
+            (u2) * p0 +
+            (2f * u * t) * p1 +
+            (t2) * p2;
+        return result;
+    }
+    public static Vector3 GetBezierCurve(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        float u = 1f - t;
+        float t2 = t * t;
+        float u2 = u * u;
+        float u3 = u2 * u;
+        float t3 = t2 * t;
+
+        Vector3 result =
+            (u3) * p0 +
+            (3f * u2 * t) * p1 +
+            (3f * u * t2) * p2 +
+            (t3) * p3;
+        return result;
+    }
 }
