7079cd9c5aa75fefea2fd5d6d3ec6c918243bfe5
SiNyong <sms3284@naver.com>
2022-11-17 17:29:49 +0900

    12. Dinamic Popup

---
+++ UnityPractice/Assets/02.Scripts/Util/Constant.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constant
+{
+    // 긍정, 부정 색상
+    public const string NormalDialogColorName = "#DEDEDE";
+    public const string PositiveDialogColorName = "#22DB5F";
+    public const string NagativeDialogColorName = "#F57C4C";
+}
